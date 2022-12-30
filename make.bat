27ee7e7e5768b004ce26e973b2a2ef2262e78d4e
mmartin46 <100044690+mmartin46@users.noreply.github.com>
2022-12-30 12:40:52 -0600

    Commit

--- make.bat
+++ make.bat
@@ -1,2 +1,6 @@
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
+
+git add .
+git commit -m "Commit"
+git push -u origin
